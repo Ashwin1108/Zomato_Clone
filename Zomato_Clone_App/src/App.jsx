14fcf0dc8eec4ae6b2797c3101d011f687dffec4
Ashwin1108 <75871066+Ashwin1108@users.noreply.github.com>
2025-01-16 13:14:57 +0530

    Created Categories Component

--- Zomato_Clone_App/src/App.jsx
+++ Zomato_Clone_App/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import HeroSection from "./Components/HeroSection";
+import Categories from "./Components/Categories";
 
 const App = () => {
   return (
     <div>
       <HeroSection />
+      <Categories />
     </div>
   );
 };
---
+++ Zomato_Clone_App/src/Components/Categories.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+const Categories = () => {
+  const [activeCategory, setActiveCategory] = useState(null);
+  const items = [
+    {
+      title: "Order Online",
+      image:
+        "https://just-eat-prod-eu-res.cloudinary.com/image/upload/c_fill,f_auto,q_auto,w_1200,h_630,d_uk:cuisines:chinese-3.jpg/v1/uk/restaurants/114123.jpg",
+    },
+    {
+      title: "Dining Out",
+      image:
+        "https://th.bing.com/th/id/OIP.xcs8S1veM-0rtGBb_VkvtgAAAA?rs=1&pid=ImgDetMain",
+    },
+    {
+      title: "Live Event",
+      image:
+        "https://th.bing.com/th/id/OIP.Tlya27DREG_SuYxgdhllYgHaE8?rs=1&pid=ImgDetMain",
+    },
+  ];
+
+  const handleCategoryClick = (index) => {
+    setActiveCategory(index);
+  };
+
+  return (
+    <section className="py-12">
+      <h2 className="text-3xl font-bold text-center mb-8">
+        Popular Categories
+      </h2>
+      <div className="flex justify-center gap-8">
+        {items.map((item, index) => (
+          <div
+            key={index}
+            className={`w-64 h-64 bg-cover bg-center rounded-lg shadow-md ${
+              activeCategory === index ? "border-4 border-red-600" : ""
+            }`}
+            style={{ backgroundImage: `url(${item.image})` }}
+            onClick={() => handleCategoryClick(index)}
+          >
+            <div className="bg-black bg-opacity-10 h-full flex items-center justify-center rounded-lg">
+              <h3 className="text-white text-xl font-semibold">{item.title}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Categories;
